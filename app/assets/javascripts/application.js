889969d7065a89d74e6345343b6c50035280c94b
Rumeysa Nur USLU <nur.uslu@bil.omu.edu.tr>
2020-05-31 12:29:26 +0300

    Add application homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,8 @@
-//= require rails-ujs
-//= require activestorage
-//= require jquery3
-//= require select2/dist/js/select2.min
-//= require owl.carousel2/dist/owl.carousel.min.js
-//= require sweetalert2/dist/sweetalert2.all
-//= require toastr/build/toastr.min
-//= require shared/toastr_config
-//= require lightgallery
-//= require lg-thumbnail
-//= require jquery-mask-plugin
-
-//= require swiper/js/swiper.min.js
-//= require clipboard/dist/clipboard.min.js
-//= require my-app
-//= require jquery.fitvids
-//= require jquery.swipebox
+//= require jquery-1.12.4.min
 //= require jquery.validate.min
-//= require classie/lib/classie.js
-//= require framework7/js/framework7.js
-//= require selectfx/selectFx.js
-
-
-$(document).ready(function(){
-  $('.datepicker').datepicker();
-});
+//= require framework7.js
+//= require jquery.swipebox
+//= require jquery.fitvids
+//= require classie.js
+//= require selectFx.js
+//= require my-app
